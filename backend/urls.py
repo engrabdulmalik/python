79b94f191c5cd41300f91efafab86be70dd077d1
engrabdulmalik <engrabdulmaliq@gmail.com>
2025-10-12 10:42:25 +0500

    CRUD

--- backend/urls.py
+++ backend/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('home',views.home,name='home'),
+    path('create_product',views.create_product,name='create_product'),
+    path('product_list',views.product_list,name='product_list'),
+    path('update_product/<int:id>',views.update_product,name='update_product'),
+    path('delete_product/<int:id>',views.delete_product,name='delete_product')
+    #127.0.0.1:8000/api/home
 ]
--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.http import HttpResponse
+from . import models
 
 # Create your views here.
+def home(request):
+    return HttpResponse("This is home page")
+
+def create_product(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        price = request.POST.get('price')
+        quantity = request.POST.get('quantity')
+        description = request.POST.get('description')
+        product = models.Product(name=name, price=price, quantity=quantity, description=description)
+        product.save()
+        return redirect('product_list')
+    return render(request, 'inventory/create_product.html')
+
+def product_list(request):
+    products = models.Product.objects.all()
+    return render(request, 'inventory/product_list.html', {'products': products})
+def update_product(request,id):
+    products=get_object_or_404(models.Product,id=id)
+    if request.method=='POST':
+        products.name=request.POST.get('name')
+        products.price=request.POST.get('price')
+        products.quantity=request.POST.get('quantity')
+        products.description=request.POST.get('description')
+        products.save()
+        return redirect('product_list')
+    return render(request,'inventory/update_product.html',{'products':products})
+
+def delete_product(request,id):
+    products=get_object_or_404(models.Product,id=id)
+    products.delete()
+    return redirect('product_list')
