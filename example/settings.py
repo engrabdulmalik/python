c03e67274cd3b4b427ec7a98d8ee7b2242dfb806
engrabdulmalik <engrabdulmaliq@gmail.com>
2025-10-19 10:55:38 +0500

    db settings

--- example/settings.py
+++ example/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'example.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'school_db',        # your database name
+        'USER': 'postgres',         # your PostgreSQL username
+        'PASSWORD': 'your_password',# your PostgreSQL password
+        'HOST': 'localhost',        # database server address
+        'PORT': '5432',
     }
 }
 
