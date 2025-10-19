e8969588268743c13e64797f83ed8306c76ca629
engrabdulmalik <engrabdulmaliq@gmail.com>
2025-10-19 09:43:19 +0500

    django panel setup

--- backend/models.py
+++ backend/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.IntegerField()
     description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
-
+    class meta:
+        permissions = [
+            ("can_change_category", "Can change category"),
+        ]
     def __str__(self):
         return self.name
